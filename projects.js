dfe0aa37f90238f5f2fc4269dba982095cde863d
ftm1337 <ftm1337@protonmail.com>
2025-03-26 01:15:10 -0400

    add bera n frens

--- projects.js
+++ projects.js
@@ -1,6 +1,4 @@
-function $(_) {return document.getElementById(_);}
-
-const DATA = {
+DATA = {
   "categories": [
     {
       "title": "Decentralized Exchanges",
@@ -10,72 +8,63 @@ const DATA = {
           "description": "Zero-Slippage Liquidity Book AMM Engine with User-Customizable Liquidity Shape creator.",
           "url": "https://eliteness.network/e3",
           "image": "https://ftm.guru/icons/e3.png",
-          "background": "linear-gradient(45deg, #07f7,#005)",
-          "chains" : ["ftm", "base", "arb1"],
+          "background": "linear-gradient(45deg, #07f7,#005)"
         },
         {
           "name": "Thick",
           "description": "Uniswap V4 inspired V3 Concentrated Liquidity fork pioneering Fee-Synthesizers as embedded hooks.",
           "url": "https://eliteness.network/thick",
           "image": "https://ftm.guru/icons/thick.png",
-          "background": "linear-gradient(45deg, #002,#555)",
-          "chains" : ["ftm", "base", "arb1"],
+          "background": "linear-gradient(45deg, #002,#555)"
         },
         {
           "name": "V.A.X.",
           "description": "<b>V</b>alue <b>A</b>dded E<b>x</b>change introduces Solidly-like Fees-Liquidity separations into a Sushi-like core.",
           "url": "https://vax.mtv.guru",
           "image": "https://ftm.guru/icons/vax.svg",
-          "background": "linear-gradient(135deg, #0033,#f03)",
-          "chains" : ["mtv"],
+          "background": "linear-gradient(135deg, #0033,#f03)"
         },
         {
           "name": "ABcDeFx.",
           "description": "<b>A</b> <b>B</b>lock<b>c</b>hain <b>D</b>ecentralized <b>E</b>xchange & <b>F</b>orE<b>x</b> is a user-friendly StableSwap AMM DEX.",
           "url": "https://abcdefx.eliteness.network",
           "image": "https://ftm.guru/icons/abcdefx.png",
-          "background": "linear-gradient(135deg, black,blue)",
-          "chains" : ["ftm", "ech", "kcc", "mtv", "kava", "sonic"],
+          "background": "linear-gradient(135deg, black,blue)"
         },
         {
           "name": "Dexchelon",
           "description": "On-chain DEX Aggregator with JIT liquidity procurement and PMM pools.",
           "url": "https://ech.eliteness.network/dexch",
           "image": "https://ftm.guru/icons/ech.svg",
-          "background": "linear-gradient(135deg, #0e0e32,#0d7676)",
-          "chains" : ["ech"],
+          "background": "linear-gradient(135deg, #0e0e32,#0d7676)"
         },
         {
           "name": "Solidly Extended",
           "description": "Solidly Extended adds support for Fee-on-Transfer tokens, Rebase, reflection & other exotic assets to Native Solidly.",
           "url": "https://ftm.guru/solidly",
           "image": "https://ftm.guru/icons/solidly.svg",
-          "background": "linear-gradient(315deg, #0e0e32,#0d7676)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(315deg, #0e0e32,#0d7676)"
         },
         {
           "name": "Equalizer Fantom Mirror",
           "description": "Alternative UI Mirror for the Equalizer.exchange Liquidity Hub on Fantom.",
           "url": "https://equal.eliteness.network",
           "image": "https://ftm.guru/icons/equal.png",
-          "background": "linear-gradient(15deg, #01011d,#076bd865)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(15deg, #01011d,#076bd865)"
         },
         {
           "name": "Equity Fantom Mirror",
           "description": "Alternative UI Mirror for the Equity.Equalizer.exchange Oracle-driven DEX on Fantom.",
           "url": "https://equity.eliteness.network",
           "image": "https://ftm.guru/icons/equity.png",
-          "background": "linear-gradient(15deg, #01011d,#076bd865)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(15deg, #01011d,#076bd865)"
         },
         {
           "name": "Equalizer Base Mirror",
           "description": "Alternative UI Mirror for the Base.Equalizer.exchange (SCALE) Liquidity Hub on Base.",
           "url": "https://equal.eliteness.network",
           "image": "https://ftm.guru/icons/scale.png",
-          "background": "linear-gradient(15deg, #01011d,#076bd865)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(15deg, #01011d,#076bd865)"
         }
       ]
     },
@@ -87,40 +76,35 @@ const DATA = {
           "description": "Liquid Wrapper & Vote Optimizer for Thena.fi's governance NFTs. <br><b>Stake ETHENA to Earn BNB.</b>",
           "url": "https://eliteness.network/ethena",
           "image": "https://ftm.guru/icons/ethena.png",
-          "background": "linear-gradient(45deg, #f05,#a0f)",
-          "chains" : ["bsc"],
+          "background": "linear-gradient(45deg, #f05,#a0f)"
         },
         {
           "name": "El Ramses",
           "description": "Liquid Wrapper and Vote Optimizer for Ramses.Exchange's governance NFTs. <br><b>Stake ELR to Earn ETH.</b>",
           "url": "https://eliteness.network/el-ramses",
           "image": "https://ftm.guru/icons/elr.png",
-          "background": "linear-gradient(225deg, goldenrod,brown)",
-          "chains" : ["arb1"],
+          "background": "linear-gradient(225deg, goldenrod,brown)"
         },
         {
           "name": "El Chronos",
           "description": "Liquid Wrapper and Vote Optimizer for Chronos.Finance's governance NFTs. <br><b>Stake elCHR to Earn ETH.</b>",
           "url": "https://eliteness.network/el-chronos",
           "image": "https://ftm.guru/icons/chr.png",
-          "background": "linear-gradient(225deg, #356ff4,purple)",
-          "chains" : ["arb1"],
+          "background": "linear-gradient(225deg, #356ff4,purple)"
         },
         {
           "name": "El Retro",
           "description": "Liquid Wrapper and Vote Optimizer for Retro.Finance's governance NFTs. <br><b>Stake elRETRO to Earn MATIC.</b>",
           "url": "https://eliteness.network/el-retro",
           "image": "https://ftm.guru/icons/elretro.png",
-          "background": "linear-gradient(135deg,  black,gray)",
-          "chains" : ["pol"],
+          "background": "linear-gradient(135deg,  black,gray)"
         },
         {
           "name": "El Snek",
           "description": "Liquid Wrapper and Vote Optimizer for Snek.Finance's governance NFTs. <br><b>Stake elSNEK to Earn AVAX.</b>",
           "url": "https://eliteness.network/el-snek",
           "image": "https://ftm.guru/icons/elsnek.png",
-          "background": "linear-gradient(315deg, #560f5c, #b60f00)",
-          "chains" : ["avax"],
+          "background": "linear-gradient(315deg, #560f5c, #b60f00)"
         }
       ]
     },
@@ -132,16 +116,14 @@ const DATA = {
           "description": "<b>Elite Lending Market Aggregator</b> can Turbocharge your Lending Market Yields by separating your Principal Tokens (PT) from its Lending Yield (virtual YT) and tapping into the Earnings Flywheel.",
           "url": "https://eliteness.network/elma",
           "image": "https://ftm.guru/icons/bank.svg",
-          "background": "linear-gradient(135deg,#777,#000)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(135deg,#777,#000)"
         },
         {
           "name": "venft.app",
           "description": "<b>venft.app</b> was world's first Lending & Borrowing platform where users could take a loan against their veNFTs such as Thena's veTHE or Retro's veRETRO.",
           "url": "https://venft.app/retro",
           "image": "https://ftm.guru/icons/solidly.svg",
-          "background": "linear-gradient(#604, #070a22)",
-          "chains" : ["bsc", "pol"],
+          "background": "linear-gradient(#604, #070a22)"
         }
       ]
     },
@@ -149,60 +131,53 @@ const DATA = {
       "title": "Yield Farming & Optimization",
       "tiles": [
         {
-          "name": "Fertilizer v1",
+          "name": "Fertilizer (Fantom)",
           "description": "Automatic Liquidity Management (ALM) Protocol with Advanced Dynamic Liquidity-Shape Management on Fantom.",
           "url": "https://eliteness.network/fertilizer",
           "image": "https://ftm.guru/icons/1f1eb.svg",
-          "background": "linear-gradient(235deg, navy, black)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(235deg, navy, black)"
         },
         {
-          "name": "Fertilizer v2",
+          "name": "Fertilizer (Base)",
           "description": "Automatic Liquidity Management (ALM) Protocol with Advanced Dynamic Liquidity-Shape Management on Base.",
           "url": "https://eliteness.network/fertilizer/base",
           "image": "https://ftm.guru/icons/1f1eb.svg",
-          "background": "linear-gradient(235deg, black, navy)",
-          "chains" : ["base"],
+          "background": "linear-gradient(235deg, black, navy)"
         },
         {
           "name": "eliteMorphexMLP",
           "description": "Wrapper on Morphex.Trade's MLP for magnified Real Yield and Automated Self-Bribing Meta-pools!",
           "url": "https://eliteness.network/elite-morphex-mlp",
           "image": "https://ftm.guru/icons/eliteMorphexMLP.png",
-          "background": "linear-gradient(-45deg, #112247,#110000)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(-45deg, #112247,#110000)"
         },
         {
           "name": "Lite Farmlands",
           "description": "Farming-as-a-Service (FaaS) platform enabling anyone to create a Farming or Staking platform for their assets.",
           "url": "https://mtv.guru/LF",
           "image": "https://ftm.guru/icons/farmer.svg",
-          "background": "linear-gradient(235deg, #0f7,#105)",
-          "chains" : ["ftm", "mtv"],
+          "background": "linear-gradient(235deg, #0f7,#105)"
         },
         {
           "name": "XELITE",
           "description": "Interest-bearing and Fractionalized ELITE compatible with tax-free DeFi applications.",
           "url": "https://eliteness.network/xelite",
           "image": "https://ftm.guru/icons/64.png",
-          "background": "linear-gradient(45deg, #fff7,#fff3)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(45deg, #fff7,#fff3)"
         },
         {
           "name": "Kompound",
           "description": "First ever Immutable & Permissionless Yield Compounder & Optimization protocol.",
           "url": "https://ftm.guru/GRAIN",
           "image": "https://ftm.guru/icons/1f33e.svg",
-          "background": "linear-gradient(45deg, brown, #da2)",
-          "chains" : ["ftm", "metis", "ech", "mtv"],
+          "background": "linear-gradient(45deg, brown, #da2)"
         },
         {
           "name": "Yield State",
           "description": "<i>Now Deprecated.</i> Yield Farming platform of ELITE liquidity providers at Sushi & Spooky!",
           "url": "https://ftm.guru/YS",
           "image": "https://ftm.guru/icons/trident.svg",
-          "background": "linear-gradient(235deg, #e900f1,#0a090a)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(235deg, #e900f1,#0a090a)"
         }
       ]
     },
@@ -214,24 +189,21 @@ const DATA = {
           "description": "Liquid-Staking Token (LST) for FTM Locked on Millenium Club Validator Nodes 37 & 63 on Fantom Opera Blockchain.",
           "url": "https://eliteness.network/mftm",
           "image": "https://ftm.guru/icons/mftm.png",
-          "background": "linear-gradient(45deg,#112,#345)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(45deg,#112,#345)"
         },
         {
           "name": "Lockless SWKAVA",
           "description": "Liquid-Staking Token (LST) for KAVA Locked on our Validator Nodes for Cosmos+EVM Kava Network Mainnet.",
           "url": "https://eliteness.network/lockless",
           "image": "https://ftm.guru/icons/swkava.svg",
-          "background": "linear-gradient(45deg,#320000,red)",
-          "chains" : ["kava"],
+          "background": "linear-gradient(45deg,#320000,red)"
         },
         {
           "name": "Lockless SWECH",
           "description": "Liquid-Staking Token (LST) for ECH Locked on our Echelon Validator Nodes.",
           "url": "https://ech.eliteness.network/lockless",
           "image": "https://ftm.guru/icons/lockless.svg",
-          "background": "linear-gradient(45deg, #360036, #b0f)",
-          "chains" : ["ech"],
+          "background": "linear-gradient(45deg, #360036, #b0f)"
         }
       ]
     },
@@ -243,24 +215,21 @@ const DATA = {
           "description": "Revolutionizing Protocol-owned Liquidity with Tokenizing Gauged Principal & Yields using On-chain Fully Dynamic Financial NFTs.",
           "url": "https://eliteness.network/elocks",
           "image": "https://ftm.guru/icons/1f510.svg",
-          "background": "linear-gradient(-180deg,#00F,#00f7)",
-          "chains" : ["ftm", "base"],
+          "background": "linear-gradient(-180deg,#00F,#00f7)"
         },
         {
           "name": "VeNFT A.M.M.",
           "description": "veN AMM is a DEX for trading EQUAL veNFTs permissionlessly, 24x7 against Fungible + Non-fungible Hybrid Liquidity pool on Fantom.",
           "url": "https://ftm.guru/equalizer",
           "image": "https://ftm.guru/icons/equal.png",
-          "background": "linear-gradient(135deg, #77f3, #7ff7)",
-          "chains" : ["ftm", "base"],
+          "background": "linear-gradient(135deg, #77f3, #7ff7)"
         },
         {
           "name": "veLiquidity.xyz",
           "description": "veLiquidity is a modern veN AMM implementation for trading SCALE veNFTs permissionlessly, 24x7 against Fungible + Non-fungible Hybrid Liquidity pool on Base.",
           "url": "https://veLiquidity.xyz",
           "image": "https://ftm.guru/icons/veliquidity.svg",
-          "background": "linear-gradient(135deg, #0d0f15, #1d2432)",
-          "chains" : ["ftm", "base"],
+          "background": "linear-gradient(135deg, #0d0f15, #1d2432)"
         }
       ]
     },
@@ -272,17 +241,14 @@ const DATA = {
           "description": "The first ever Casino on Fantom, born in 2021! First Telegram-to-Blockchain GameFi Dapp on Fantom.",
           "url": "https://ftm.guru/degam",
           "image": "https://ftm.guru/icons/m2.png",
-          "background": "linear-gradient(45deg, #000, #022627)",
-          "chains" : ["ftm"],
+          "background": "linear-gradient(45deg, #000, #022627)"
         },
         {
           "name": "Kucino Casino",
           "description": "12+ Gaming Titles for immersive GambleFi experience on the Kucoin Chain!",
           "url": "https://kcc.eliteness.network/kucino",
           "image": "https://ftm.guru/icons/kucino.png",
-          "background": "linear-gradient(to bottom,rgba(0,255,127,1),rgba(1,240,255,1))",
-          "color": "#000 !important",
-          "chains" : ["kcc"],
+          "background": "linear-gradient(to bottom,rgba(0,255,127,1),rgba(1,240,255,1))"
         }
       ]
     },
@@ -292,13 +258,11 @@ const DATA = {
         {
           "name": "Compare Solidly Forks",
           "url": "https://eliteness.network/compare-solidly",
-          "image": "https://ftm.guru/icons/solidly.svg",
           "background": "linear-gradient(45deg,#320000,red)"
         },
         {
           "name": "Compare Liquid Wrappers",
           "url": "https://eliteness.network/compare-wrappers",
-          "image": "https://ftm.guru/icons/solidly.svg",
           "background": "linear-gradient(45deg,#320000,red)"
         }
       ]
@@ -336,12 +300,6 @@ const DATA = {
           "background": "linear-gradient(45deg,#000,#012)"
         },
         {
-          "name": "Treasury Tracker!",
-          "url": "https://eliteness.network/treasury",
-          "image": "https://ftm.guru/icons/1f451.svg",
-          "background": "linear-gradient(45deg,#fff,#7af)"
-        },
-        {
           "name": "Interact with our community on Discord!",
           "url": "https://discord.gg/QpyfMarNrV",
           "image": "https://ftm.guru/icons/discord.svg",
@@ -356,133 +314,4 @@ const DATA = {
       ]
     }
   ]
-}
-
-
-
-
-const CHAINS = {
-  //[
-	"eth" : {
-		"image" : "https://ftm.guru/icons/eth.svg"
-	},
-	"bsc" : {
-		"image" : "https://ftm.guru/icons/bnb.svg"
-	},
-	"arb1" : {
-		"image" : "https://ftm.guru/icons/arb1.svg"
-	},
-	"ftm" : {
-		"image" : "https://ftm.guru/icons/ftm.svg"
-	},
-	"ech" : {
-		"image" : "https://ftm.guru/icons/ech.svg"
-	},
-	"kava" : {
-		"image" : "https://ftm.guru/icons/kava.svg"
-	},
-	"mtv" : {
-		"image" : "https://ftm.guru/icons/mtv.png"
-	},
-	"kcc" : {
-		"image" : "https://ftm.guru/icons/kcs.svg"
-	},
-	"sonic" : {
-		"image" : "https://ftm.guru/icons/sonic.png"
-	},
-	"pol" : {
-		"image" : "https://ftm.guru/icons/matic.svg"
-	},
-	"avax" : {
-		"image" : "https://ftm.guru/icons/avax.svg"
-	},
-	"base" : {
-		"image" : "https://ftm.guru/icons/base.svg"
-	},
-	"metis" : {
-		"image" : "https://ftm.guru/icons/metis.svg"
-	},
-  //]
-}
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    dexstats();
-    paintTiles(DATA);
-});
-
-async function dexstats() {
-	return;
-}
-
-function paintTiles(data) {
-  const container = document.createElement('div');
-
-  data.categories.forEach(category => {
-    const categoryTitle = document.createElement('h2');
-    categoryTitle.className = 'cat-h2';
-    categoryTitle.textContent = category.title;
-    container.appendChild(categoryTitle);
-
-    const tilesWrapper = document.createElement('div');
-    tilesWrapper.className = 'wrapper-tiles';
-
-    category.tiles.forEach(tile => {
-      const tileDiv = document.createElement('div');
-      tileDiv.className = 'wrapper-tile';
-
-      const tileColor = (tile.color) ? tile.color : "white";
-      tileDiv.style.background = tile.background;
-      tileDiv.style.color = tileColor;
-
-      if(tile.url) {
-        tileDiv.onclick = () => window.open(tile.url);
-      }
-
-      const tileTitle = document.createElement('div');
-      tileTitle.className = 'wrapper-tile-title';
-
-
-      if(tile.image) {
-        const tileImage = document.createElement('img');
-        tileImage.src = tile.image;
-        tileImage.alt = tile.name;
-        tileTitle.appendChild(tileImage);
-      }
-
-      const titleText = document.createElement('div');
-      titleText.innerHTML = `<div class="subhead-big" style="color:${tileColor};">${tile.name}</div>`;
-
-
-	  const chainsWrapper = document.createElement('div');
-      if(tile.chains) {
-		chainsWrapper.className = 'subhead-small wrapper-chains';
-        tile.chains.forEach(chain => {
-          const chainImg = document.createElement('img');
-          chainImg.src = CHAINS[chain].image;
-          chainsWrapper.appendChild(chainImg);
-        });
-        titleText.appendChild(chainsWrapper);
-      }
-
-      tileTitle.appendChild(titleText);
-
-
-      tileDiv.appendChild(tileTitle);
-
-
-      const tileDesc = document.createElement('div');
-      tileDesc.className = 'wrapper-desc';
-      tileDesc.style.color = tileColor;
-      tileDesc.innerHTML = tile.description ? tile.description : "" ;
-
-      tileDiv.appendChild(tileDesc);
-      tilesWrapper.appendChild(tileDiv);
-    });
-
-    container.appendChild(tilesWrapper);
-  });
-
-  document.getElementById('mainstage').appendChild(container);
 }
